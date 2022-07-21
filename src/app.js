beff5addc0c1412f59259055b71b09b8a32733bd
Natasha0611 <lunataly06@gmail.com>
2022-07-21 18:28:10 +0100

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,126 @@
+function formatDate(timestamp){
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours<10){
+        hours = `0${hours}`}
+    let minutes = date.getMinutes();
+    if (minutes<10){
+        minutes = `0${minutes}`
+    }
+    let days = ["Sunday", "Monday" ,"Tuesday", "Wensday", "Thursday", "Friday","Saturday"]
+let day = days[date.getDay()];
+return `${day} ${hours}:${minutes}`;
+}
+
+function formatDay(timestamp){
+let date = new Date(timestamp*1000);
+let day = date.getDay();
+let days = ["Sun", "Mon","Tue","Wen","Thu","Fri","Sat"];
+
+return days[day];
+}
+
+
+function displayForecast(response){
+    let forecast = response.data.daily;
+    let forecastElement = document.querySelector("#forecast");
+    let forecastHTML = `<div class="row">`;
+    forecast.forEach(function(forecastDay,index) {
+        if(index<6){
+        forecastHTML = forecastHTML+
+        `
+        <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+         <img 
+            src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+            alt=""
+            width="42"
+         />
+         <div class="weather-forecast-temperature">
+        <span class="weather-forecast-temperature-max">
+        ${Math.round(forecastDay.temp.max)}°
+        </span>
+        <span class="weather-forecast-temperature-min">  
+        ${Math.round(forecastDay.temp.min)}°
+        </span>  
+    </div>
+</div>
+`;}
+    });
+    forecastHTML = forecastHTML+`</div>`;
+    forecastElement.innerHTML = forecastHTML;
+}
+function getForecast(coordinates){
+    console.log(coordinates);
+    let apiKey = `907fef83425ee6575ce345a2f87d8989`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`
+    console.log(apiUrl);
+    axios.get(apiUrl).then(displayForecast);
+}
+
+
+function displayTemperature(response){
+celsiusTemperature = response.data.main.temp;
+let temperatureElement = document.querySelector(`#temperature`);
+temperatureElement.innerHTML = Math.round(celsiusTemperature);
+let cityElement = document.querySelector(`#city`);
+cityElement.innerHTML = response.data.name;
+let descriptionElement = document.querySelector(`#description`);
+descriptionElement.innerHTML = response.data.weather[0].description;
+let humidityElement = document.querySelector(`#humidity`);
+humidityElement.innerHTML = response.data.main.humidity;
+let windElement = document.querySelector(`#wind`);
+windElement.innerHTML = Math.round(response.data.wind.speed);
+let precipitationElement = document.querySelector(`#precipitation`);
+precipitationElement.innerHTML = response.data.main.humidity;
+let dateElement = document.querySelector(`#date`);
+dateElement.innerHTML = formatDate(response.data.dt*1000);
+let iconElement = document.querySelector(`#icon`);
+iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+iconElement.setAttribute("alt", document.querySelector(`#description`));
+
+getForecast(response.data.coord);
+}
+
+function search(city){
+let apiKey = `907fef83425ee6575ce345a2f87d8989`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
+
+
+}
+
+
+function  handleSubmit(event){
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+function displayFahrenheitTemperature(event){
+    event.preventDefault();
+    celsiuslink.classList.remove("active");
+    fahrenheitlink.classList.add("active");
+    let fahrenheitTemperature = (celsiusTemperature* 9 / 5) + 32;
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+    }
+
+function displayCelsiusTemperature(event){
+    event.preventDefault(); 
+    celsiuslink.classList.add("active");
+    fahrenheitlink.classList.remove("active");
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+let fahrenheitlink = document.querySelector("#fahrenheit-link");
+fahrenheitlink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiuslink = document.querySelector("#celsius-link");
+celsiuslink.addEventListener("click", displayCelsiusTemperature);
+search("Kyiv");
